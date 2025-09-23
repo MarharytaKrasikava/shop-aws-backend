3e016ea27f3af34f2c475834733b49997bcc8b98
Marharyta Krasikava <marharyta_krasikava@epam.com>
2025-09-23 23:48:39 +0300

    Initial commit

---
+++ test/shop-backend-empty.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopBackendEmpty from '../lib/shop-backend-empty-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-backend-empty-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopBackendEmpty.ShopBackendEmptyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
